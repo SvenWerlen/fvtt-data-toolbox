0a362706d2a98a4ec0955080033a8406c8f829d2
Sven Werlen <sven.werlen@pm.me>
2021-03-24 20:31:06 -0400

    v-1.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.8.0] - 2020-03-24
+### Added
+- Let's Contribute: support for accept/reject/archive status
+- Insert new entries into compendiums
+- Update existing entries into compendiums
+- Batch merge into compendiums
+
 ## [1.7.0] - 2020-03-17
 ### Added
 - Sample for importing items (#11 Missing item-template and items-sample)
--- change-version-fix.sh
+++ change-version-fix.sh
@@ -3,9 +3,9 @@ MAJVER=`grep version module.json | awk -F'"' '{ print $4}' | awk -F'.' '{ print 
 MINVER=`grep version module.json | awk -F'"' '{ print $4}' | awk -F'.' '{ print $2}'`
 FIXVER=`grep version module.json | awk -F'"' '{ print $4}' | awk -F'.' '{ print $3}'`
 #echo "Before: $MAJVER.$MINVER.$FIXVER"
-NEWMINVER="$(($MINVER+1))"
-sed -i "s/$MAJVER\.$MINVER\.$FIXVER/$MAJVER\.$NEWMINVER\.0/g" module.json
-VERSION="v-$MAJVER.$NEWMINVER.0"
+NEWFIXVER="$(($FIXVER+1))"
+sed -i "s/$MAJVER\.$MINVER\.$FIXVER/$MAJVER\.$MINVER\.$NEWFIXVER/g" module.json
+VERSION="v-$MAJVER.$MINVER.$NEWFIXVER"
 
 git add module.json
 echo "git commit -m \"$VERSION\" && git tag \"$VERSION\" && git push && git push --tags"
